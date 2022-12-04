0e99fab748221de92d91ba93261a8b01158305c6
Letícia 'Lev' Leitão <99267278+leticialevleitao@users.noreply.github.com>
2022-12-04 13:52:33 -0300

    60% do curso Validações, buscas, filtros e deploy

---
+++ clientes/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from clientes.models import Cliente
+from clientes.validators import *
+
+class ClienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cliente
+        fields = '__all__'
+   
+    def validate(self,data):
+        if not cpf_valido(data['cpf']):
+           raise serializers.ValidationError({'cpf':"CPF inválido"})
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':'Nome inválido'})
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError({'rg':'RG deve ter 9 digitos'})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular':'Celular inválido'})
+        return data
+ 
---
+++ clientes/validators.py
@@ -0,0 +1,16 @@
+from validate_docbr import CPF
+
+def cpf_valido(numero_cpf):
+    cpf = CPF()
+    return cpf.validate(numero_cpf)
+        
+def nome_valido(nome):
+    return nome.isalpha()
+
+def rg_valido(rg):
+    return len(rg) == 9
+        
+def celular_valido(celular):
+    return len(celular) ==11
+      
+    
